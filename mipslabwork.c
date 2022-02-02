84e59ce130f9e725f7182576253b729becc821b4
Erik Jonsson <fallowshades@gmail.com>
2022-02-02 16:18:00 +0100

    Can i get it right?

--- mipslabwork.c
+++ mipslabwork.c
@@ -36,9 +36,9 @@ void labwork( void )
 {
   delay( 1000 );
   time2string( textstring, mytime );
-  display_string( 3, textstring );
+ // display_string( 3, textstring );
   display_update();
   tick( &mytime );
-  display_image(96, icon);
+  //display_image(96, icon);
 }
 #pragma endregion
